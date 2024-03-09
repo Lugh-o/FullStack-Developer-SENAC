5110c9be282ec0e9a284cad4b51bbccf0cbf5178
Lugho-o <lughfalcao@gmail.com>
2024-03-09 04:43:57 -0300

    guanabara 09/03

---
+++ SENAC/ExerciciosAulas13e14pt2/Q2.js
@@ -0,0 +1,5 @@
+function contarDigitos(integer){
+    return integer.toString().length
+}
+
+console.log(contarDigitos(30))
---
+++ SENAC/ExerciciosAulas13e14pt2/Q4.js
@@ -0,0 +1,8 @@
+let input = 15
+
+if(input % 3 === 0 && input % 5 === 0){
+    console.log(`O número ${input} é múltiplo de 3 e 5 ao mesmo tempo`);
+} else{
+    console.log(`O número ${input} não é múltiplo de 3 e 5 ao mesmo tempo`);
+}
+
