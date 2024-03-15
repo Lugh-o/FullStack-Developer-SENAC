2463237e3943728bb3ed7df77e46c0bc7f7d5cda
Lugh-o <lughfalcao@gmail.com>
2024-03-15 15:48:50 -0300

    guanabara 15/03

---
+++ Guanabara/HTML5-CSS3/Modulo4/4-MediaQuery/mq005/script.js
@@ -0,0 +1,8 @@
+function mobileMenu() {
+    let x = document.getElementById("imenu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
